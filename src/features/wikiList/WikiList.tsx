b00f0aa36d292766acf262fa26d1469936e12b56
Alexandros Ntitorss <a.ntitoras@gmail.com>
2022-09-09 11:58:31 +0300

    wiki list

--- src/features/wikiList/WikiList.tsx
+++ src/features/wikiList/WikiList.tsx
@@ -1,13 +1,45 @@
 import React from "react";
 
 import { useWikiPeople } from "./hooks";
+import { Flex, Spinner } from "@chakra-ui/react";
+
+import { Alert } from "src/components";
+import { Table } from "./components";
 
 const WikiList = () => {
-  const { people } = useWikiPeople();
+  const { people, isLoading, peopleError, handleSelectPerson } =
+    useWikiPeople();
 
-  console.log({ people });
+  if (isLoading)
+    return (
+      <Flex alignItems="center" justifyContent="center">
+        <Spinner size="lg"></Spinner>
+      </Flex>
+    );
 
-  return <div>Hello</div>;
+  if (peopleError) {
+    return (
+      <Alert
+        title="Error At fetching Coins Market"
+        description="An error occured while fetching alert"
+      ></Alert>
+    );
+  }
+
+  return (
+    <Flex
+      alignItems="center"
+      flexDirection="column"
+      w="100%"
+      p={4}
+      color="white"
+    >
+      <Table
+        people={people || []}
+        handleSelectPerson={handleSelectPerson}
+      ></Table>
+    </Flex>
+  );
 };
 
 export default WikiList;
